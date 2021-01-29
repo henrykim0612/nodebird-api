4449a219dabf44af0c4a6b412e236423db1ac523
Henry.Kim <hyeongjong90@gmail.com>
2021-01-29 12:45:08 +0900

    프로젝트 생성

---
+++ passport/kakaoStrategy.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const KakaoStrategy = require('passport-kakao').Strategy;
+
+const User = require('../models/user');
+
+module.exports = () => {
+  passport.use(new KakaoStrategy({
+    clientID: process.env.KAKAO_ID,
+    callbackURL: '/auth/kakao/callback',
+  }, async (accessToken, refreshToken, profile, done) => {
+    console.log('kakao profile', profile);
+    try {
+      const exUser = await User.findOne({where: {snsId: profile.id, provider: 'kakao'}});
+      if (exUser) {
+        done(null, exUser); // 이미 있으므로 통과
+      } else {
+        // 사용자 저장
+        const newUser = await User.create({
+          email: profile._json && profile._json.kakao_account_email,
+          nick: profile.displayName,
+          snsId: profile.id,
+          provider: 'kakao',
+        });
+        done(null, newUser);
+      }
+    } catch (err) {
+      console.error(err);
+      done(error);
+    }
+  }));
+};
